455c7badd7e60f3d5b8db205ed949aa5a27496f1
Luca Casartelli <lc.casartelli@gmail.com>
2015-05-28 19:40:42 +0200

    updates

--- app/src/main/java/it/unimib/relibrary/controllers/NotificationsFragment.java
+++ app/src/main/java/it/unimib/relibrary/controllers/NotificationsFragment.java
@@ -2,6 +2,8 @@ package it.unimib.relibrary.controllers;
 
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +15,54 @@ public class NotificationsFragment extends REFragment {
 
     private static final String TITLE = "Notifiche";
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     public static String getTitle() {
         return TITLE;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.notifications_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mRecyclerView = (RecyclerView) this.getView().findViewById(R.id.notification_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] myDataset = new String[] {
+            "Matteo ti ha aggiunto agli amici",
+            "Maria è una pessima fotografa",
+            "Aurora ha chiesto di uscire con te",
+            "Matteo ti ha aggiunto agli amici",
+            "Maria è una pessima fotografa",
+            "Aurora ha chiesto di uscire con te",
+            "Matteo ti ha aggiunto agli amici",
+            "Maria è una pessima fotografa",
+            "Aurora ha chiesto di uscire con te",
+            "Matteo ti ha aggiunto agli amici",
+            "Maria è una pessima fotografa",
+            "Aurora ha chiesto di uscire con te",
+            "Matteo ti ha aggiunto agli amici",
+            "Maria è una pessima fotografa",
+            "Aurora ha chiesto di uscire con te",
+            "Matteo ti ha aggiunto agli amici",
+            "Maria è una pessima fotografa",
+            "Aurora ha chiesto di uscire con te",
+            "Matteo ti ha aggiunto agli amici",
+            "Maria è una pessima fotografa",
+            "Aurora ha chiesto di uscire con te"
+        };
+
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
