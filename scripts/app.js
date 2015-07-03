2c6975c999a71e9f582d5885060d235dc44d7b2c
Giaco9 <giacomo.consonni@gmail.com>
2015-07-03 13:18:33 +0200

    show toast in detail book

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,34 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
+(function(window) {
+  'use strict';
+  window.getQueryStringValue = function(key) {
+    var search = window.location.href.split('?')[1];
+    var _r_val;
+    if(search !== undefined) {
+      var eachVariables = search.split('&');
+      var pairs = null;
+      eachVariables.forEach(function(variables) {
+        pairs = variables.split('=');
+        pairs.forEach(function(val, index) {
+          if(index % 2 === 0 && val === key) {
+            _r_val = pairs[index + 1];
+          }
+        });
+      });
+    }
+    return _r_val;
+  };
+
+  window.showToast = function() {
+    var queryStringValue = window.getQueryStringValue('toast');
+    if(queryStringValue !== undefined) {
+      var toast = document.querySelector('#' + queryStringValue);
+      if(toast !== undefined) {
+        toast.show();
+      }
+    }
+  };
+
+})(window);
